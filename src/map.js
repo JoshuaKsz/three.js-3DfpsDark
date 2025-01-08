e3efbd74744d4471be99352d6c299755bc87aac4
SelinVelday <2272048@maranatha.ac.id>
2025-01-08 09:45:00 +0700

    map

---
+++ src/map.js
@@ -0,0 +1,51 @@
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import CubeTest from "./cube_test.js";
+import * as CANNON from 'cannon-es';
+
+
+export default class Map{
+    constructor(scene, world) {
+        const loader = new GLTFLoader();
+
+        let model = null;
+        loader.load('./models/map/dust2/dust2.gltf', (gltf) => {
+            model = gltf;
+            
+            model.scene.scale.set(2.25, 2.25, 2.25);
+            model.scene.position.set(0, 0, 0);
+            scene.add(model.scene);
+            console.log(model);
+
+            // scene.remove(this.model);
+
+        });
+        console.log(model);
+        // this.model.dispose();
+        console.log("a",model);
+        
+
+        
+        this.collisionMap = [
+            new CubeTest(world, scene, new CANNON.Vec3(-42, 3.8, 24.7), new CANNON.Vec3(26.7, 0.25, 6), 0x00ff00, 0), // lantai
+            new CubeTest(world, scene, new CANNON.Vec3(-27.87, 6.9, 28.6), new CANNON.Vec3(1, 3.69, 2.8), 0x00ff00, 0), // wall z
+            new CubeTest(world, scene, new CANNON.Vec3(-27.87, 9.35, 23), new CANNON.Vec3(1, 1.3, 2.99), 0x00ff00, 0),
+            new CubeTest(world, scene, new CANNON.Vec3(-27.87, 6.9, 14), new CANNON.Vec3(1, 3.69, 6.3), 0x00ff00, 0),
+            new CubeTest(world, scene, new CANNON.Vec3(-21.2, 6.9, 18.75), new CANNON.Vec3(5.85, 3.69, 0.5), 0x00ff00, 0), // wall x
+            new CubeTest(world, scene, new CANNON.Vec3(-30.7, 5.7, 17.3), new CANNON.Vec3(2, 2, 2), 0x00ff00, 0),
+            new CubeTest(world, scene, new CANNON.Vec3(-29.8, 5.7, 14.4), new CANNON.Vec3(1, 2, 1), 0x00ff00, 0),
+            new CubeTest(world, scene, new CANNON.Vec3(32.1, 3.93, -90.83), new CANNON.Vec3(1, 2, 1), 0x00ff00, 0),
+            new CubeTest(world, scene, new CANNON.Vec3(39.44, 6.46, -92.64), new CANNON.Vec3(9, 4, 1), 0x00ff00, 0),
+            new CubeTest(world, scene, new CANNON.Vec3(53.84, 5.73, -69.37), new CANNON.Vec3(4, 5, 0.5), 0x00ff00, 0),
+            new CubeTest(world, scene, new CANNON.Vec3(54.11, 4.44, -61.39), new CANNON.Vec3(0.5, 5, 11), 0x00ff00, 0),
+            new CubeTest(world, scene, new CANNON.Vec3(48.30, 5.77, -81.27), new CANNON.Vec3(0.5, 5, 11), 0x00ff00, 0),
+            new CubeTest(world, scene, new CANNON.Vec3(30.32, 7.08, -87.90), new CANNON.Vec3(0.5, 3.5, 4), 0x00ff00, 0),
+        ];
+    }
+
+    allCollisionPhysicsUpdate() {
+        this.collisionMap.forEach(item => {
+            item.update();
+        });
+    }
+
+};
